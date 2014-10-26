2232425653068aabd87204569a66004abbc14767
Stephen Starkey <scstarkey@gmail.com>
2014-10-26 14:05:57 -0500

    Add role.

--- spec/freefrog/governance_spec.clj
+++ spec/freefrog/governance_spec.clj
@@ -2,6 +2,9 @@
   (:require [speclj.core :refer :all]
             [freefrog.governance :as g]))
 
+(def sample-anchor (g/anchor-circle "Sample"))
+(def role-name "Programmer")
+
 (describe "Anchor Circle"
   (it "can create an anchor circle with a lead link"
     (should= {:name      "Courage Labs"
@@ -10,7 +13,7 @@
              (g/anchor-circle "Courage Labs" "Stephen Starkey"
                               "stephen@couragelabs.com"))
 
-    (should= {:name "Fear Labs" :lead-link {:name "Bill O'Reilly"
+    (should= {:name "Fear Labs" :lead-link {:name  "Bill O'Reilly"
                                             :email "billoreilly@foxnews.com"}}
              (g/anchor-circle "Fear Labs" "Bill O'Reilly"
                               "billoreilly@foxnews.com")))
@@ -42,5 +45,54 @@
                   (g/anchor-circle nil))
     (should-throw IllegalArgumentException "Name may not be empty"
                   (g/anchor-circle ""))))
+
+(let [purpose "Building awesome software"
+      sample-anchor-with-role (g/add-role sample-anchor role-name purpose)
+      domains ["Code" "Tests"]
+      accountabilities ["Writing Code" "Testing their own stuff"]]
+  (describe "Roles"
+    (it "can add a role to a circle with name and purpose"
+      (should= (assoc sample-anchor :roles {role-name {:purpose purpose}})
+               sample-anchor-with-role))
+
+    (it "can add a role to a circle that already has roles"
+      (should= (update-in sample-anchor-with-role [:roles] assoc "Tester"
+                          {:purpose "Making sure Programmers don't screw up"})
+               (g/add-role sample-anchor-with-role "Tester"
+                           "Making sure Programmers don't screw up")))
+
+    (it "can add a role to a circle with name and accountabilities"
+      (should= (assoc sample-anchor :roles
+                      {role-name {:accountabilities accountabilities}})
+               (g/add-role sample-anchor role-name
+                           nil nil accountabilities)))
+
+    (it "can add a role to a circle with name, purpose, and domains"
+      (should= (assoc sample-anchor :roles {role-name {:domains domains}})
+               (g/add-role sample-anchor role-name
+                           nil domains nil)))
+
+    (it "can add a role to a circle with name, purpose, and accountabilities"
+      (should= (assoc sample-anchor :roles {role-name {:domains domains}})
+               (g/add-role sample-anchor role-name
+                           nil domains nil)))
+
+    (it "can add a role to a circle with everything"
+      (should= (assoc sample-anchor :roles
+                      {role-name {:purpose          purpose
+                                  :domains          domains
+                                  :accountabilities accountabilities}})
+               (g/add-role sample-anchor role-name purpose domains
+                           accountabilities)))
+
+    (it "throws errors when doing bad things"
+      (should-throw IllegalArgumentException "Name may not be empty"
+                    (g/add-role sample-anchor nil nil nil nil))
+      (should-throw IllegalArgumentException "Name may not be empty"
+                    (g/add-role sample-anchor "" nil nil nil))
+      (should-throw IllegalArgumentException (str "Role already exists: "
+                                               role-name)
+                    (g/add-role sample-anchor-with-role role-name nil nil
+                                nil)))))
 
 (run-specs)
--- src/clj/freefrog/governance.clj
+++ src/clj/freefrog/governance.clj
@@ -12,3 +12,28 @@
    (let [anchor-circle (anchor-circle name)]
      (assoc anchor-circle :lead-link
             {:name lead-link-name :email lead-link-email}))))
+
+(defn- add-if [map key value]
+  (if value
+    (assoc map key value)
+    map))
+
+(defn- make-role [purpose domains accountabilities]
+  (-> {}
+      (add-if :purpose purpose)
+      (add-if :domains domains)
+      (add-if :accountabilities accountabilities)))
+
+(defn add-role
+  ([circle role-name purpose]
+   (add-role circle role-name purpose nil nil))
+  ([circle role-name purpose domains accountabilities]
+   (if (empty? role-name)
+     (throw (IllegalArgumentException. "Name may not be empty")))
+   (if (get-in circle [:roles role-name])
+     (throw (IllegalArgumentException. (str "Role already exists: " role-name))))
+   (let [circle (if (contains? circle :roles)
+                  circle
+                  (assoc circle :roles {}))]
+     (update-in circle [:roles] assoc role-name
+                (make-role purpose domains accountabilities)))))
