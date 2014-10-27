7fe60ba48af4c215113c0bda12ef4064e0db1e6e
Stephen Starkey <scstarkey@gmail.com>
2014-10-27 07:29:13 -0500

    Annotated specs with sections of the Constitution that they support.

--- spec/freefrog/governance_roles_spec.clj
+++ spec/freefrog/governance_roles_spec.clj
@@ -11,6 +11,7 @@
 (def sample-domains ["Code" "Tests"])
 (def sample-accountabilities ["Writing Code" "Testing their own stuff"])
 
+;; Section 3.1.a
 (describe "Role Manipulation"
   (describe "adding"
     (it "can add a role to a circle with name and purpose"
@@ -47,7 +48,8 @@
         (g/add-role sample-anchor role-name sample-purpose sample-domains
                     sample-accountabilities)))
 
-    (it "doesn't let you create any of the special roles")
+    ;; Section 2.5
+    (it "doesn't let you create any of the constitutionally defined roles")
 
     (it "doesn't let you use empty names"
       (should-throw IllegalArgumentException "Name may not be empty"
@@ -99,8 +101,10 @@
           (should-throw IllegalArgumentException "No role specified to update"
             (g/rename-role sample-anchor-with-role "" new-name)))
 
+        ;; Section 2.2.3 and 2.5.3 (not specified!)
         (it "doesn't let you rename any of the special roles")))
 
+    ;; Section 1.1.a
     (describe "purpose"
       (it "can change a role's purpose"
         (let [new-purpose "Building software that's grrreat!"]
@@ -128,27 +132,47 @@
         (should-throw IllegalArgumentException "No role specified to update"
           (g/update-role-purpose sample-anchor-with-role "" "Stuff")))
 
+      ;; Section 2.5.3
       (it "doesn't let you change the purpose of the special roles"))
 
+    ;; Section 1.1.b
     (describe "domains"
+
+      ;; Section 2.2.3
       (it "doesn't let you add domains to the Lead Link")
+
+      ;; Section 2.5.3
       (it "allows you to add/remove domains to/from any of the elected roles")
       (it "doesn't let you update/remove any of the constitutional domains of
         the elected roles"))
 
+    ;; Section 1.1.c
     (describe "accountabilities"
+      ;; Section 2.2.3
       (it "doesn't let you add accountabilities to the Lead Link")
+
+      ;; Section 2.5.3
       (it "allows you to add/remove accountabilities to/from any of the
         elected roles")
       (it "doesn't let you update/remove any of the constitutional
         accountabilities of the elected roles")))
 
   (describe "assigning"
+    ;; Section 2.4
     (it "can assign someone to a role")
+
+    ;; Section 2.4.3
     (it "can remove someone from a role")
+
+    ;; Section 2.4.2
     (it "can assign multiple people to a role")
-    (it "can only assign one person each to the elected roles")
+    (it "can assign multiple people to a role with focuses")
+
+    ;; Section 2.5.1
     (it "won't assign the person in the Lead Link role to the Facilitator
-    role")))
+    or Rep Link role")
+
+    ;; Section 2.5.2
+    (it "can assign one person each to an elected role, with a term")))
 
 (run-specs)
