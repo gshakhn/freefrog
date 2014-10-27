3a660593310b4e60bcc3bec047c7339324238f0b
Stephen Starkey <scstarkey@gmail.com>
2014-10-27 06:44:07 -0500

    Extracted roles into two namespaces.

---
+++ spec/freefrog/governance_anchor_circle_spec.clj
@@ -0,0 +1,43 @@
+(ns freefrog.governance-anchor-circle-spec
+  (:require [freefrog.governance :as g]
+            [speclj.core :refer :all]))
+
+(describe "Anchor Circle"
+  (it "can create an anchor circle with a lead link"
+    (should= {:name      "Courage Labs"
+              :lead-link {:name  "Stephen Starkey"
+                          :email "stephen@couragelabs.com"}}
+      (g/anchor-circle "Courage Labs" "Stephen Starkey"
+                       "stephen@couragelabs.com"))
+
+    (should= {:name "Fear Labs" :lead-link {:name  "Bill O'Reilly"
+                                            :email "billoreilly@foxnews.com"}}
+      (g/anchor-circle "Fear Labs" "Bill O'Reilly"
+                       "billoreilly@foxnews.com")))
+
+  (it "can create an anchor circle without a lead link"
+    (should= {:name "Courage Labs"} (g/anchor-circle "Courage Labs")))
+
+  (it "doesn't let you leave any parameters empty"
+    (should-throw IllegalArgumentException "Name may not be empty"
+      (g/anchor-circle nil))
+    (should-throw IllegalArgumentException "Name may not be empty"
+      (g/anchor-circle ""))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle nil nil nil))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle "" nil nil))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle "" "Joe" "joescmoe@here.com"))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle "Not Enough Information" nil nil))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle "Not Enough Information" nil
+                       "joeschmoe@here.com"))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle "Not Enough Information" ""
+                       "joeschmoe@here.com"))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle "Not Enough Information" "Joe Schmoe" nil))
+    (should-throw IllegalArgumentException "No parameters may be empty"
+      (g/anchor-circle "Not Enough Information" "Joe Schmoe" ""))))
--- spec/freefrog/governance_roles_spec.clj
+++ spec/freefrog/governance_roles_spec.clj
@@ -1,4 +1,4 @@
-(ns freefrog.governance-spec
+(ns freefrog.governance-roles-spec
   (:require [clojure.set :as s]
             [freefrog.governance :as g]
             [speclj.core :refer :all]))
@@ -10,46 +10,6 @@
 (def sample-anchor-with-role (g/add-role sample-anchor role-name sample-purpose))
 (def sample-domains ["Code" "Tests"])
 (def sample-accountabilities ["Writing Code" "Testing their own stuff"])
-
-(describe "Anchor Circle"
-  (it "can create an anchor circle with a lead link"
-    (should= {:name      "Courage Labs"
-              :lead-link {:name  "Stephen Starkey"
-                          :email "stephen@couragelabs.com"}}
-      (g/anchor-circle "Courage Labs" "Stephen Starkey"
-                       "stephen@couragelabs.com"))
-
-    (should= {:name "Fear Labs" :lead-link {:name  "Bill O'Reilly"
-                                            :email "billoreilly@foxnews.com"}}
-      (g/anchor-circle "Fear Labs" "Bill O'Reilly"
-                       "billoreilly@foxnews.com")))
-
-  (it "can create an anchor circle without a lead link"
-    (should= {:name "Courage Labs"} (g/anchor-circle "Courage Labs")))
-
-  (it "doesn't let you leave any parameters empty"
-    (should-throw IllegalArgumentException "Name may not be empty"
-      (g/anchor-circle nil))
-    (should-throw IllegalArgumentException "Name may not be empty"
-      (g/anchor-circle ""))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle nil nil nil))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle "" nil nil))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle "" "Joe" "joescmoe@here.com"))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle "Not Enough Information" nil nil))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle "Not Enough Information" nil
-                       "joeschmoe@here.com"))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle "Not Enough Information" ""
-                       "joeschmoe@here.com"))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle "Not Enough Information" "Joe Schmoe" nil))
-    (should-throw IllegalArgumentException "No parameters may be empty"
-      (g/anchor-circle "Not Enough Information" "Joe Schmoe" ""))))
 
 (describe "Role Manipulation"
   (describe "adding"
@@ -68,7 +28,7 @@
                       {role-name {:accountabilities sample-accountabilities}})
         (g/add-role sample-anchor role-name
                     nil nil sample-accountabilities)))
-
+Ex
     (it "can add a role to a circle with name, purpose, and domains"
       (should= (assoc sample-anchor :roles {role-name {:domains sample-domains}})
         (g/add-role sample-anchor role-name
