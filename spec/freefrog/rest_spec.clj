7fb31cb5cd7fddc5765823aa6aebc2fd319d0b8f
Bill <william.finn@gmail.com>
2014-11-02 14:11:14 -0600

    Handle POST requests to circles resource.

--- spec/freefrog/rest_spec.clj
+++ spec/freefrog/rest_spec.clj
@@ -6,12 +6,25 @@
 (describe "governance rest api"
   (before-all (r/start-test-server))
   (after-all (r/stop-test-server))
+  (after (r/reset-database))
 
   (it "should return status code 404 at the root"
     (should= 404 (:status (http-client/get "http://localhost:3000" {:throw-exceptions false}))))
 
   (context "when no circles have been created"
-    (with response (http-client/get "http://localhost:3000/circle" {:throw-exceptions false}))
+    (with response (http-client/get "http://localhost:3000/circles" {:throw-exceptions false}))
     (it "should return an empty array"
         (should= 200 (:status @response))
-        (should= "[]" (:body @response)))))
+        (should= "[]" (:body @response))))
+
+  (context "upon creating a circle"
+    (with response (http-client/post "http://localhost:3000/circles" 
+                                    {:throw-exceptions false
+                                     :content-type :json
+                                     :form-params {:name "Test Circle!"}}))
+    (it "should redirect"
+      (should= 303 (:status @response))
+      (should-contain "application/json" (get-in @response [:headers "Content-Type"]))
+      (should-contain "location" (:body @response)))
+    (context "and getting the created circle"
+      (it "should return the content that was created" ))))
--- src/clj/freefrog/rest.clj
+++ src/clj/freefrog/rest.clj
@@ -5,7 +5,8 @@
   (:use ring.adapter.jetty)
   (:import java.net.URL))
 
-(defonce entries (ref {}))
+(defonce circles (ref {}))
+
 (defn build-entry-url [request id]
   (URL. (format "%s://%s:%s%s/%s"
                 (name (:scheme request))
@@ -14,13 +15,34 @@
                 (:uri request)
                 (str id))))
 
-(defresource circle-resource
+(defresource circle-resource [id]
+  :allowed-methods [:get :put :delete]
+  :exists? (fn [_]
+             (let [e (get @circles id)]
+                    (if-not (nil? e)
+                      {::entry e})))
+  :existed? (fn [_] (nil? (get @circles id ::sentinel)))
   :available-media-types ["application/json"]
-  :allowed-methods [:get]
+  :handle-created ::entry
+  :delete! (fn [_] (dosync (alter circles assoc id nil)))
+  :can-put-to-missing? false
+  :put! #(dosync (alter circles assoc id (::data %)))
+  :new? (fn [_] (nil? (get @circles id ::sentinel))))
+
+(defresource circles-resource
+  :available-media-types ["application/json"]
+  :allowed-methods [:get :post]
+  :post! #(let [id (str (inc (rand-int 100000)))]
+            (dosync (alter circles assoc id (::data %)))
+                 {::id id})
+  :post-redirect? true
   :handle-ok #(map (fn [id] (str (build-entry-url (get % :request) id)))
-                   (keys @entries)))
+                   (keys @circles))
+  :location (fn [ctx] {:location (format "/circles/%s" (::id ctx))}))
+
 (defroutes app
-  (ANY "/circle" [] circle-resource))
+  (ANY "/circles/:id" [id] (circle-resource id))
+  (ANY "/circles" [] circles-resource))
 
 (def handler 
   (-> app 
@@ -36,3 +58,7 @@
 
 (defn stop-test-server []
   (.stop @test-server))
+
+(defn reset-database []
+  (dosync
+    (ref-set circles {})))
