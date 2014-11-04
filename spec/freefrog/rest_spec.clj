fb414fcf6c8fd350e8c931bc77eea63bb478aae0
Bill <william.finn@gmail.com>
2014-11-04 07:21:02 -0600

    Refactor test to be more DRY.

--- spec/freefrog/rest_spec.clj
+++ spec/freefrog/rest_spec.clj
@@ -4,12 +4,26 @@
             [clj-http.client :as http-client]
             [freefrog.rest :as r]))
 
+(defn http-post-request 
+  ([uri body]
+   (http-post-request uri body nil))
+  ([uri body options]
+   (http-client/post (str "http://localhost:3000/" uri) 
+                     (merge {:throw-exceptions false
+                             :content-type :json
+                             :body body} options))))
+
+(defn http-get-request 
+  ([]
+   (http-get-request "" nil))
+  ([uri]
+   (http-get-request uri nil))
+  ([uri options]
+   (http-client/get (str "http://localhost:3000/" uri) (merge {:throw-exceptions false} options))))
+
 (defn should-return-400 [spec-context circle-content response-content]
   (context spec-context
-    (with response (http-client/post "http://localhost:3000/circles" 
-                                    {:throw-exceptions false
-                                     :content-type :json
-                                     :body circle-content}))
+    (with response (http-post-request "circles" circle-content))
     (it "should return a bad request"
       (should= 400 (:status @response))
       (should-contain response-content (:body @response)))))
@@ -20,23 +34,21 @@
   (after (r/reset-database))
 
   (it "should return status code 404 at the root"
-    (should= 404 (:status (http-client/get "http://localhost:3000" {:throw-exceptions false}))))
+    (should= 404 (:status (http-get-request))))
 
   (context "circles"
     (context "when no circles have been created"
-      (with response (http-client/get "http://localhost:3000/circles" {:throw-exceptions false}))
+      (with response (http-get-request "circles"))
       (it "should return an empty array"
           (should= 200 (:status @response))
           (should= "[]" (:body @response))))
   
     (context "when creating a circle"
-      (with response (http-client/post "http://localhost:3000/circles" 
-                                      {:throw-exceptions false
-                                       :content-type :json
-                                       :body (json/generate-string 
-                                               {:name "Test Circle!"
-                                                :lead-link-name "Bill"
-                                                :lead-link-email "bfinn@example.com"})}))
+      (with response (http-post-request "circles" 
+                                        (json/generate-string 
+                                          {:name "Test Circle!"
+                                           :lead-link-name "Bill"
+                                           :lead-link-email "bfinn@example.com"})))
       (it "should redirect to the location of the newly created resource"
         (should= 303 (:status @response))
         (should-contain "application/json" (get-in @response [:headers "Content-Type"]))
@@ -54,18 +66,14 @@
   
     (context "when creating and retrieving the created circle"
       (with location (get (json/parse-string 
-                            (:body (http-client/post 
-                                     "http://localhost:3000/circles" 
-                                     {:throw-exceptions false
-                                      :content-type :json
-                                      :body (json/generate-string 
-                                              {:name "Test Circle!"
-                                               :lead-link-name "Bill"
-                                               :lead-link-email "bfinn@example.com"})})))
+                            (:body (http-post-request
+                                     "circles" 
+                                     (json/generate-string 
+                                       {:name "Test Circle!"
+                                        :lead-link-name "Bill"
+                                        :lead-link-email "bfinn@example.com"}))))
                             "location"))
-      (with get-response (http-client/get (str "http://localhost:3000" 
-                                               @location)
-                                      {:throw-exceptions false}))
+      (with get-response (http-get-request @location))
       (it "should return the content that was created" 
           (should= 200 (:status @get-response))
           (should= {"lead-link" {"email" "bfinn@example.com"
@@ -73,21 +81,21 @@
                    (json/parse-string (:body @get-response)))))
   
     (context "when creating multiple circles"
-      (before (http-client/post "http://localhost:3000/circles" 
-                                      {:content-type :json
-                                       :body (json/generate-string 
-                                               {:name "Test Circle!"
-                                                :lead-link-name "Bill"
-                                                :lead-link-email "bfinn@example.com"})}))
+      (before (http-post-request "circles" 
+                                 (json/generate-string 
+                                   {:name "Test Circle!"
+                                    :lead-link-name "Bill"
+                                    :lead-link-email "bfinn@example.com"})
+                                 {:throw-exceptions true}))
 
-      (before (http-client/post "http://localhost:3000/circles" 
-                                      {:content-type :json
-                                       :body (json/generate-string 
-                                               {:name "Test Circle 2"
-                                                :lead-link-name "Bill"
-                                                :lead-link-email "bfinn@example.com"})}))
-      (with get-response (http-client/get "http://localhost:3000/circles" 
-                                          {:throw-exceptions false}))
+      (before (http-post-request "circles" 
+                                 (json/generate-string 
+                                   {:name "Test Circle 2"
+                                    :lead-link-name "Bill"
+                                    :lead-link-email "bfinn@example.com"})
+                                 {:throw-exceptions true}))
+
+      (with get-response (http-get-request "circles"))
 
       (it "should return an array of created circles"
         (should= 200 (:status @get-response))
@@ -97,48 +105,39 @@
 
   (context "roles"
     (context "when requesting the list of roles for a non-existent circle"
-      (with roles-response (http-client/get (str "http://localhost:3000/circles/1234/roles")
-                                            {:throw-exceptions false}))
+      (with roles-response (http-get-request "circles/1234/roles"))
         (it "should 404"
           (should= 404 (:status @roles-response))))
 
     (context "when posting a new role to a non-existent circle"
-      (with roles-response (http-client/post 
-                             (str "http://localhost:3000/circles/1234/roles")
-                             {:throw-exceptions false
-                              :content-type :json
-                              :body (json/generate-string 
-                                      {:name "My Role!"})}))
+      (with roles-response (http-post-request 
+                             "circles/1234/roles"
+                             (json/generate-string {:name "My Role!"})))
         (it "should 400 with a helpful error message"
             (should= 400 (:status @roles-response))
             (should-contain "Circle 1234 does not exist." (:body @roles-response))))
 
     (context "with a new circle"
       (with circle-location (get (json/parse-string 
-                            (:body (http-client/post 
-                                     "http://localhost:3000/circles" 
-                                     {:throw-exceptions false
-                                      :content-type :json
-                                      :body (json/generate-string 
-                                              {:name "Test Circle!"
-                                               :lead-link-name "Bill"
-                                               :lead-link-email "bfinn@example.com"})})))
-                            "location"))
+                                   (:body (http-post-request
+                                            "circles" 
+                                             (json/generate-string 
+                                                     {:name "Test Circle!"
+                                                      :lead-link-name "Bill"
+                                                      :lead-link-email "bfinn@example.com"}))))
+                                 "location"))
 
         (context "when requesting the list of roles"
-          (with roles-response (http-client/get (str "http://localhost:3000" @circle-location "/roles")
-                                                {:throw-exceptions false}))
+          (with roles-response (http-get-request (str @circle-location "/roles")))
           (it "should return an empty array"
               (should= 200 (:status @roles-response))
               (should= "[]" (:body @roles-response))))
 
         (context "creating a role"
-          (with roles-response (http-client/post 
-                                 (str "http://localhost:3000" @circle-location "/roles")
-                                 {:throw-exceptions false
-                                  :content-type :json
-                                  :body (json/generate-string 
-                                          {:name "My Role!"})}))
+          (with roles-response (http-post-request
+                                 (str @circle-location "/roles")
+                                 (json/generate-string 
+                                          {:name "My Role!"})))
           (it "should redirect to the location of the newly created resource"
               (should= 303 (:status @roles-response))
               (should-contain "application/json" 
